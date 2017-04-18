06f57ba38751d8a588d6442c60f24a07a216a011
古家大 <yannwennri2@gmail.com>
2017-04-18 18:19:22 +0900

    add command

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+# 個別のJSをアセットパイプラインに追加する
 Rails.application.config.assets.precompile += %w(*.js)
