be5647a6603cabdf8df574feb0ec534422201e47
古家大 <yannwennri2@gmail.com>
2017-04-18 18:16:03 +0900

    react initialize

--- frontend/src/javascripts/app.js
+++ frontend/src/javascripts/app.js
@@ -1,5 +1,15 @@
-class SampleClass {
-  constructor() {
-    console.log('test')
+import React from 'react'
+import { render } from 'react-dom'
+
+class SampleApp extends React.Component {
+  render() {
+    return (
+      <h1>
+        Hello World!
+      </h1>
+    )
   }
 }
+
+const elem = React.createElement(SampleApp)
+render(elem, document.getElementById('sample'))
