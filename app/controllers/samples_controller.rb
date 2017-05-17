d075849876fca46d9e3a0437f462f24da622abd2
古家大 <yannwennri2@gmail.com>
2017-05-17 14:19:50 +0900

    add tiqav api

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,4 +1,13 @@
 class SamplesController < ApplicationController
   def index
   end
+
+  def fetch_random_tiqav_images
+    res = Faraday.get 'http://api.tiqav.com/search/random.json'
+    images = JSON.parse(res.body)
+    random_images = images.map do |image|
+      { id: image['id'], imgUrl: "http://img.tiqav.com/#{image['id']}.#{image['ext']}" }
+    end
+    render json: random_images
+  end
 end
