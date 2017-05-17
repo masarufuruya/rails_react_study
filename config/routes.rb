a302c824339d4d8c4b00aaf1f9daa37760adc0d1
古家大 <yannwennri2@gmail.com>
2017-05-17 14:35:53 +0900

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'samples#index'
-  resources :samples, only: [:index]
+  resources :samples, only: [:index] do
+    collection do
+      get :fetch_random_tiqav_images
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
